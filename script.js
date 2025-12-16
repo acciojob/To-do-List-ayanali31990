aaf3bc752c35fdce602b9beb0d7de0ff1bb6aea8
Ayan Ali <149358183+ayanali31990@users.noreply.github.com>
2025-12-16 10:27:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
 
+//your code here
+
+const input = document.getElementById("newTodoInput");
+const btn = document.getElementById("addTodoBtn");
+const list = document.getElementById("todoList");
+
+btn.addEventListener("click", function () {
+  let value = input.value.trim();
+
+  // Do not allow empty input
+  if (value === "") {
+    return;
+  }
+
+  // Create new li
+  let li = document.createElement("li");
+  li.textContent = value;
+
+  // Append to list
+  list.appendChild(li);
+
+  // Clear input
+  input.value = "";
+});
